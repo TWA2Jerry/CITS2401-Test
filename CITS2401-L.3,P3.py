74446e42a178d7caca08bb012a39a37425ca2a72
Jesse Zhou <jessezhou@Jesses-Air.bloomlab>
2018-10-26 10:30:21 +0800

    Some change to L.3 apparentely

--- CITS2401-L.3,P3.py
+++ CITS2401-L.3,P3.py
@@ -45,3 +45,47 @@ def spiral(a, b, h, n):
     ax.plot(x_array,y_array,z_array, label='Elliptical Spiral')
     ax.legend()
     plt.show()
+
+data = np.array([-0.2,1.0,3.5,-3,2])
+data[np.where(data < 0)]*=-1
+print(data)
+
+myType = [('name','U20'),('height',int),('age',int)]
+values = [('Billy',178,18),('Beatrice',182,19),('Bob',160,17)]
+students = np.array(values,dtype=myType)
+
+print(np.sort(students,order = 'height'))
+
+
+data = np.array([(u'Ford', 1.8, u'blue'),(u'Holden', 2.0, u'silver'),(u'Hyundai', 1.6, u'blue')],
+dtype=[('make','U10'),('capacity',float),('colour','U10')])
+
+print(data[np.where(data[np.where(data['colour']=='blue')]['capacity']>1.6)])
+
+#print(data[np.where(data['colour']=='blue' and data['capacity']>=1.6)]) Doesn't work because we need either a.any or a.all
+
+# print(data[np.where(data[colour==blue] and data[capacity>1.6])]) doesn't work because colour isn't defined
+
+# print(data[np.where(data['colour']=='blue')] and data[np.where(data['capacity']>1.6)])
+# data[np.where(data[colour==blue] and data[capacity>1.6])]
+# print(data[np.where(data['colour']=='blue' and data['capacity']>=1.6)])'''
+
+def position(theta1,theta2,l1,l2):
+    vert = math.sin(theta1)*l1+math.sin(theta2+theta1)*l2
+    hor = math.cos(theta1)*l1+math.cos(theta2+theta1)*l2
+    print(hor,vert)
+    
+def polygon(offset):
+    offset = math.radians(offset)
+    angles = np.arange(0,2*math.pi,math.pi/3)
+    sines = np.sin(angles+offset)
+    cosines = np.cos(angles+offset)
+    print(cosines,sines)
+    plt.plot(cosines,sines)
+    plt.show()
+    
+a = np.array([[1,2,3],
+             [4,5,6]])
+a = a+1
+print(a)
+
